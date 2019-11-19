18898e344b1675c7cab3be013877ddf123cc2d14
justty32 <justty32@gmail.com>
2019-11-19 13:30:46 +0800

    ff

--- Core.cs
+++ Core.cs
@@ -25,14 +25,29 @@ namespace GameCore
         // Gamecore
         // about GetSet, Script, Module, SaveLoad... 
         // only reset while start the program
-        public static Core Instance { get; } = new Core();
+        private static Core p_instance = null;
+        public static Core Instance {
+            get{
+                if(p_instance == null)
+                    p_instance = new Core();
+                return p_instance;
+            }
+        }
+        public static void InstanceRemove(bool are_you_sure = false){ if(are_you_sure){ p_instance = null; }}
 
         // World data
         // about world's rules, datas....
         // reset while load a new world
-
-        //TODO: make a function, to init the rules
-        //just a place holder
-        public Rules rules { get; } = new Rules(new Root.TimeRule.Time(), 0);
+        public void DataRemove(){
+            _rules = null;
+        }
+        public void DataInit( // many parameters
+            Root.TimeRule.Time now_time,
+            int LocationRule_location_number_distribute_reference            
+        ){
+            _rules = new Rules(now_time, LocationRule_location_number_distribute_reference);
+        }
+        private Rules _rules = null;
+        public Rules rules { get => _rules; }
     }
 }
